9758eb1b7485d3fecdcb9501e22279f0564fec46
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2023-05-30 17:54:40 -0300

    helm and projectile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -44,7 +44,9 @@
     ;; smex
 
     ;; project navigation
-    ;; projectile
+    projectile
+
+    helm-projectile
 
     ;; git integration
     magit))
@@ -73,13 +75,25 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
+;; PROJECTILE
+(use-package projectile
+  :ensure t
+  :config
+  (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+  (projectile-mode +1))
+
+(use-package helm-projectile
+  :ensure t
+  :config (helm-projectile-on))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(deeper-blue))
- '(package-selected-packages '(clojure-mode use-package magit evil-surround)))
+ '(package-selected-packages
+   '(helm-projectile projectile clojure-mode use-package magit evil-surround)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
