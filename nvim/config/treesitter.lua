9dcf1ec44183a0b8093ae20af22cd396b0c27a1b
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2023-12-28 15:51:31 -0300

    add dockerfile in treesitter

--- nvim/config/treesitter.lua
+++ nvim/config/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed =  { "make", "typescript", "javascript", "c", "lua", "vim", "vimdoc", "sql", "clojure", "java", "json", "json5", "make", "bash" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed =  { "dockerfile", "make", "typescript", "javascript", "c", "lua", "vim", "vimdoc", "sql", "clojure", "java", "json", "json5", "make", "bash" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   highlight = {
     enable = true,
