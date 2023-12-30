7f9aca7259264a9831e8373b3b9ed898d6ed1ec0
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2023-12-30 20:20:22 -0300

    snippets

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -8,9 +8,13 @@ return
   -- these dependencies will only be loaded when cmp loads
   -- dependencies are always lazy-loaded unless specified otherwise
   dependencies = {
-    'hrsh7th/cmp-path', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-cmdline'
+    'hrsh7th/cmp-path', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-cmdline',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+    "rafamadriz/friendly-snippets"
   },
   config = function()
+    require("luasnip.loaders.from_vscode").lazy_load()
     local cmp = require('cmp')
 
     cmp.setup.cmdline('/', {
@@ -29,6 +33,16 @@ return
     })
 
     return cmp.setup({
+      snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
       mapping = {
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
         ["<Tab>"] = cmp.mapping.select_next_item(),
@@ -42,6 +56,7 @@ return
           { name = "nvim_lsp" },
           { name = "path" },
           { name = "buffer" },
+          { name = 'luasnip' },
         })
     })
   end
