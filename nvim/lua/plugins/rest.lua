8b4ce3e7e40fe194efff11740ceb97c093e2cd90
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-01-03 22:08:41 -0300

    ok: remove jq just workws wtf

--- nvim/lua/plugins/rest.lua
+++ nvim/lua/plugins/rest.lua
@@ -33,7 +33,6 @@ return {
         -- executables or functions for formatting response body [optional]
         -- set them to false if you want to disable them
         formatters = {
-          json = "jq .",
           html = function(body)
             return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
           end
