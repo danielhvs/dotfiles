e3033acae298849d6cf61dd2089156bde3117b55
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-01-04 11:36:15 -0300

    border

--- nvim/lua/plugins/git-messenger.lua
+++ nvim/lua/plugins/git-messenger.lua
@@ -4,7 +4,7 @@ return {
     vim.cmd [[
 " <leader>gm is the default mapping
 let g:git_messenger_always_into_popup = v:true
-let g:git_messenger_floating_win_opts = { 'border': 'solid' }
+let g:git_messenger_floating_win_opts = { 'border': 'single' }
 " let g:git_messenger_include_diff = "current"
 ]]
   end
