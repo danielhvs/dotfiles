2a85ee61e8c9b073aa07c68e2164c3cb58a1c469
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-01-06 19:31:46 -0300

    lua

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,50 +1,3 @@
-vim.o.background = "dark"
-vim.cmd [[
-set clipboard=unnamedplus
-set numberwidth=1 
-set scrolloff=1
-set hidden
-set nohlsearch
-set lazyredraw
-set splitright
-set splitbelow
-set nobackup
-set nowritebackup
-set noswapfile
-set smarttab
-set expandtab
-set wrap 
-set noerrorbells
-set novisualbell
-set incsearch 
-set nonumber
-set iskeyword+=-
-set ignorecase
-set smartcase
-set relativenumber
-set number
-set path+=**
-set listchars=tab:→\ ,trail:·
-set nolist
-set mouse=
-]]
-
--- " for `gf` to work well
--- " tim pope sensible plugin https://github.com/tpope/vim-sensible/blob/master/plugin/sensible.vim
-vim.cmd[[
-set wildignore+=**/node_modules/**,**/target/**
-set autoread
-syntax enable
-filetype plugin indent on
-setglobal tags-=./tags tags-=./tags; tags^=./tags;
-set noshowmode 
-let g:loaded_perl_provider = 0
-set cursorline
-au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-
-au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=150}
-]]
-
 local g = vim.g
 g.maplocalleader = "-"
 g.mapleader = ","
@@ -56,3 +9,46 @@ o.shiftwidth = 2
 o.tabstop = 2
 o.autoindent = true
 o.smartindent = true
+o.background = "dark"
+o.clipboard = "unnamedplus"
+o.scrolloff = 1
+o.hidden = true
+o.lazyredraw = true
+o.hlsearch = false
+o.splitright = true
+o.splitbelow = true
+o.backup = false
+o.writebackup = false
+o.smarttab = true
+o.expandtab = true
+o.wrap = true
+o.errorbells = false
+o.visualbell = false
+o.incsearch = true
+o.numberwidth = 1
+o.number = true
+o.relativenumber = true
+o.list = false
+o.mouse = ''
+o.smartcase = true
+o.ignorecase = true
+o.autoread = true
+o.showmode = false
+o.cursorline = true
+
+vim.cmd [[
+set iskeyword+=-
+set path+=**
+set listchars=tab:→\ ,trail:·
+]]
+
+-- " for `gf` to work well
+-- " tim pope sensible plugin https://github.com/tpope/vim-sensible/blob/master/plugin/sensible.vim
+vim.cmd [[
+set wildignore+=**/node_modules/**,**/target/**
+filetype plugin indent on
+setglobal tags-=./tags tags-=./tags; tags^=./tags;
+let g:loaded_perl_provider = 0
+au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=150}
+]]
