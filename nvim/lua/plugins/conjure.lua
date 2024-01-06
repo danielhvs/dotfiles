49b4f37087e4b99dcae8fc0a39ebce7e6c2d5ab6
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-01-06 19:41:03 -0300

    conjure->lua

--- nvim/lua/plugins/conjure.lua
+++ nvim/lua/plugins/conjure.lua
@@ -1,24 +1,16 @@
 return {
   'Olical/conjure',
   config = function()
-    vim.cmd [[
-let g:conjure#log#hud#enabled = v:false
-let g:conjure#relative_file_root = getcwd()
-let g:conjure#mapping#doc_word = "K"
-let g:conjure#client#clojure#nrepl#eval#auto_require = v:false
-let g:conjure#client#clojure#nrepl#connection#auto_repl#enabled = v:false
-let g:conjure#filetype#sql = v:false
-let g:conjure#log#wrap = v:true
-
-autocmd User ConjureEval if expand("%:t") =~ "^conjure-log-" | exec "normal G" | endif
-
-" use these mappings! -lv -lq
-" use these mappings! -ece -ecr -ecw (comments)
-nnoremap <leader>ll :ConjureLogVSplit<CR>:set wrap<CR><localleader>ll
-
-let g:conjure#eval#inline#highlight = "clojureFunc"
-let g:conjure#eval#inline#prefix = "=> "
-
-]]
+    --  use these mappings! -lv -lq
+    --  use these mappings! -ece -ecr -ecw (comments)
+    vim.g['conjure#log#hud#enabled'] = false
+    vim.g['conjure#mapping#doc_word'] = "K"
+    vim.g['conjure#relative_file_root'] = vim.fn.getcwd()
+    vim.g['conjure#client#clojure#nrepl#eval#auto_require'] = false
+    vim.g['conjure#client#clojure#nrepl#connection#auto_repl#enabled'] = false
+    vim.g['conjure#filetype#sql'] = false
+    vim.g['conjure#log#wrap'] = true
+    vim.g['conjure#eval#inline#highlight'] = 'clojureFunc'
+    vim.g['conjure#eval#inline#prefix'] = '=> '
   end
 }
