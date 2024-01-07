a33ce4f933dab9bc4073d3fb348b7f9615aa5b98
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-01-07 12:39:04 -0300

    git-messenger to lua

--- nvim/lua/plugins/git-messenger.lua
+++ nvim/lua/plugins/git-messenger.lua
@@ -1,11 +1,7 @@
 return {
   'rhysd/git-messenger.vim',
   config = function()
-    vim.cmd [[
-" <leader>gm is the default mapping
-let g:git_messenger_always_into_popup = v:true
-let g:git_messenger_floating_win_opts = { 'border': 'single' }
-" let g:git_messenger_include_diff = "current"
-]]
+    vim.g.git_messenger_always_into_popup = true
+    vim.g.git_messenger_floating_win_opts = { border = 'single' }
   end
 }
