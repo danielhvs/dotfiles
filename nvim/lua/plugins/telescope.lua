720d92f60d8142760609068eecdfecccfba5e632
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-01-14 00:43:13 -0300

    elix

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -19,7 +19,7 @@ return {
           path_display = { "smart" },
           initial_mode = "normal",
           prompt_prefix = "",
-          file_ignore_patterns = { "node_modules", "DS_Store", "target" },
+          file_ignore_patterns = { "node_modules", "DS_Store", "target", "deps" },
           layout_config = {
             horizontal = { width = 0.95 },
             vertical = { width = 0.95 }
