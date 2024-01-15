ba6f71d7a3489138ae9682581f9d7f4cc1faeba9
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-01-15 09:35:41 -0300

    no icons

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -23,7 +23,10 @@ return {
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_b = { 'branch', 'diff',
+          { 'diagnostics',
+            symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' } },
+        },
         lualine_c = { 'filename' },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
