bb99008ad870dfccfe71e2dba4317bf0ce76d15e
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-02-29 08:50:29 -0300

    better illuminate

--- nvim/lua/plugins/illuminate.lua
+++ nvim/lua/plugins/illuminate.lua
@@ -61,17 +61,17 @@ return {
 
 
     -- change the highlight style
-    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Cursor" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Cursor" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Cursor" })
 
     --- auto update the highlight style on colorscheme change
     vim.api.nvim_create_autocmd({ "ColorScheme" }, {
       pattern = { "*" },
       callback = function(_ev)
-        vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-        vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
-        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Cursor" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Cursor" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Cursor" })
       end
     })
   end
