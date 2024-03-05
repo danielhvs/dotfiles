5c38a4ab44708a60503a84597a64c1f3a6009a51
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-03-05 08:39:09 -0300

    Directory ficou bom no illuminate

--- nvim/lua/plugins/illuminate.lua
+++ nvim/lua/plugins/illuminate.lua
@@ -64,18 +64,8 @@ return {
 
 
     -- change the highlight style
-    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "IncSearch" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "IncSearch" })
-    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "IncSearch" })
-
-    --- auto update the highlight style on colorscheme change
-    vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-      pattern = { "*" },
-      callback = function(_ev)
-        vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "IncSearch" })
-        vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "IncSearch" })
-        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "IncSearch" })
-      end
-    })
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Directory" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Directory" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Directory" })
   end
 }
