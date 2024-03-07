0cf814d55e88aa59d0f9cedb1d01b443423699d2
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-03-07 18:27:22 -0300

    clojurestorm

--- dev/user.clj
+++ dev/user.clj
@@ -1,9 +1,12 @@
 (ns dev.user
   (:require
-   [clojure.tools.nrepl.server :as nrepl]))
+   [clojure.tools.nrepl.server :as nrepl]
+   [flow-storm.api :as fs-api]))
 
 (defonce nrepl-server (nrepl/start-server))
 (spit "./.nrepl-port" (:port nrepl-server))
 
 (require 'pjstadig.humane-test-output)
 (pjstadig.humane-test-output/activate!)
+
+(comment (fs-api/local-connect))
